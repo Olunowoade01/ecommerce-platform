a1979d045db2f3c05f66095cd6ff5ffc2f39175b
Olunowoade01 <adeoyerichard344@gmail.com>
2025-07-06 13:43:32 +0100

    ecommerce-platform

--- api/index.js
+++ api/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/products', (req, res) => res.json([{ id: 1, name: "Sample Product" }]));
+
 app.listen(3001, () => console.log('API running on port 3001'));
