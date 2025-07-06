a42b33dd5bdee62dd4eaf5f3138c46dada6e906f
Olunowoade01 <adeoyerichard344@gmail.com>
2025-07-06 13:15:30 +0100

    ecommerce-platform

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://ecommerce-platform-j4ve.onrender.com/products')
+      .then(res => res.json())
+      .then(setProducts)
+      .catch(console.error);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Products</h1>
+      <ul>
+        {products.map(p => (
+          <li key={p.id}>{p.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
